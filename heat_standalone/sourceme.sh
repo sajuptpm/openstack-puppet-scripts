778a56cffee95d895b5c891d03c92b4d6d073f92
Saju Madhavan <sajuptpm@gmail.com>
2014-07-09 20:16:27 +0530

    Added comments

--- heat_standalone/sourceme.sh
+++ heat_standalone/sourceme.sh
@@ -4,12 +4,12 @@
 
 HEATCLIENT_DEBUG=0
 
-#Don't use admin user and admin tenant
+#Don't use admin user and admin tenant, issue:VM will not get private IP.
 OS_USERNAME=demo
 OS_PASSWORD=nova
 #Endpoint of keystone
 OS_AUTH_URL=http://192.168.56.103:35357/v2.0
-#Don't use admin tenant
+#Don't use admin tenant, issue:VM will not get private IP.
 OS_TENANT_NAME=demo
 #run "#keystone tenant-list" and find the IP of tenant
 OS_TENANT_ID=a45e1c5b562942a5a3af8cf302ae2b47
